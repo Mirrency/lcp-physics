b4f94257600d1d27638091ab38445e92c5e703ff
XuanweiLiu <1878443249@qq.com>
2026-07-16 17:18:52 +0800

    build: target Python 3.11 and PyTorch 2.7.1

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     platforms=['any'],
     url='https://github.com/locuslab/lcp-physics',
     packages=find_packages(exclude=['demos', 'videos']),
-    install_requires=['py3ode', 'pygame']
+    python_requires='>=3.11,<3.12',
+    install_requires=[
+        'torch==2.7.1',
+        'numpy==1.26.4',
+        'pygame==2.6.1',
+    ],
+    extras_require={'test': ['pytest==8.4.1']},
 )
